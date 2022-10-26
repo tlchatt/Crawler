9d3ad06ea481282370984bebd2b543b70ae1112c
Greggory Wiley <greggorywiley@tlchatt.com>
2022-10-26 07:47:29 -0400

    Cufrrent push

--- Controller.js
+++ Controller.js
@@ -6,10 +6,12 @@ import {MigratePageContent} from './components/MigratePageContent.js'
 async function Controller (){
     var targetDomain = 'https://www.forrestjacksonlaw.com/'
     var targetSitemap = 'https://www.forrestjacksonlaw.com/sitemap.xml'
-   // var URLList = await EnumerateURLList(targetDomain,targetSitemap) ///Enumerates all intenral urls linked from homepage and sitemap
-  var URLList = [
-    'https://www.forrestjacksonlaw.com/about-us/meet-our-founder/',
-]  
+    var URLList = await EnumerateURLList(targetDomain,targetSitemap) ///Enumerates all intenral urls linked from homepage and sitemap
+    /* For testing single url
+    var URLList = [
+        'https://www.forrestjacksonlaw.com/about-us/meet-our-founder/',
+    ]  
+    */
    var MigrateResult = await MigratePageContent(targetDomain,URLList)
     console.log(MigrateResult)
 }
